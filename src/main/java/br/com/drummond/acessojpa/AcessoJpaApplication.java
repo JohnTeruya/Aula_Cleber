ee44f0ff0823171878c0c385f07a819a7cdea3ad
JohnTeruya <jteruya9@gmail.com>
2018-05-08 20:51:19 -0300

    Projeto Inicial

---
+++ src/main/java/br/com/drummond/acessojpa/AcessoJpaApplication.java
@@ -0,0 +1,12 @@
+package br.com.drummond.acessojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcessoJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcessoJpaApplication.class, args);
+	}
+}
